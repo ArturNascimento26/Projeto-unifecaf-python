d360cbe9276be2a330dc69e7e6ed7a193712578b
Tutu2611 <arturfilhinho70@gmail.com>
2026-03-25 11:41:56 -0300

    Iniciando projeto

---
+++ projeto.py
@@ -0,0 +1,9 @@
+print("\nGestão de Peças, Qaulidade e Armazenamento")
+print("1. Cadastrar nova peça")
+print("2. Listar peças aprovadas/reprovadas")
+print("3. Remover peça cadastrada")
+print("4. Listar caixas fechadas")
+print("5. Gerar relatório final")
+print("6. Sair")
+
+opcao = input("Digite a opção desejada: ")
