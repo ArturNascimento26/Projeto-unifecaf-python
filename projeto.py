bd6cebd8de7fff10d06dd376129d3f65618f19a8
Tutu2611 <arturfilhinho70@gmail.com>
2026-03-26 09:17:08 -0300

    cadastrar peça

--- projeto.py
+++ projeto.py
@@ -1,3 +1,10 @@
+def cadastrar_peca():
+  print("Preencha os dados da peça que deseja cadastrar")
+  id_peca = input("Qual o id da peça? ")
+  peso_peca = int(input("Qual o peso da peça? "))
+  cor_peca = input("Qual a cor da peça? [azul/verde] ")
+  comprimento_peca = int(input("Qual o comprimento da peça? "))
+
 print("\nGestão de Peças, Qaulidade e Armazenamento")
 print("1. Cadastrar nova peça")
 print("2. Listar peças aprovadas/reprovadas")
